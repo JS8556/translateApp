02fac973d9df7fd2fdd29bf1ae0978f87aca6f51
JS8556 <sykora.jakub11@seznam.cz>
2017-10-08 17:55:38 +0200

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,6 +9,8 @@ import { TranslationData } from '../../providers/translation-data';
 export class HomePage {
 
   private textForTranslation:String = '';
+  private langFrom:String = '';
+  private langTo:String = '';
   private cardContent:String = '';
 
   constructor(public navCtrl: NavController, private translation: TranslationData) {
@@ -19,13 +21,17 @@ export class HomePage {
    * user input
    * @param tText 
    */
-  public translateClick(tText:String){
+  public translateClick(tText:String, from:String, to:String){
     this.textForTranslation = tText;
+    this.langFrom = from;
+    this.langTo = to;
 
     console.log(this.textForTranslation);
+    console.log(this.langFrom);
+    console.log(this.langTo);
 
     // pass text for translation to translation service
-    this.translation.getTranslation(this.textForTranslation).subscribe( (result) => {
+    this.translation.getTranslation(this.textForTranslation, this.langFrom, this.langTo).subscribe( (result) => {
       this.cardContent = result.responseData.translatedText;
     });
   }
